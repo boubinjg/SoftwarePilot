260c662355de80e3aece9a537a5811d280d2b986
boubinjg <boubinjg@gmail.com>
2019-03-20 13:24:26 -0400

    Transfering Codebase to Github

---
+++ AUAVAndroid/app/src/main/java/org/reroutlab/code/auav/drivers/AuavDrivers.java
@@ -0,0 +1,31 @@
+package org.reroutlab.code.auav.drivers;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+import org.eclipse.californium.core.CoapServer;
+import java.util.HashMap;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.eclipse.californium.core.CoapClient;
+import org.eclipse.californium.core.CoapResponse;
+import org.eclipse.californium.core.Utils;
+import org.eclipse.californium.core.CoapHandler;
+
+
+import java.util.HashMap;
+
+abstract public class AuavDrivers {
+	private long startTime=0;
+	public long getStartTime() {
+		return startTime;
+	}
+	public void setStartTime(long millis) {
+		startTime = millis;
+	}
+
+	abstract public int getLocalPort();
+	abstract public String getUsageInfo();
+	abstract public void setDriverMap(HashMap<String,String> m);
+	abstract public void setLogLevel(Level l);
+	abstract public CoapServer getCoapServer();
+}
