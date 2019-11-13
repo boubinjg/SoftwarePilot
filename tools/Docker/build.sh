1dde40e7162e4148955155edbd0079df0161148d
Nat Shineman <natshineman@gmail.com>
2019-11-13 16:53:20 -0500

    Remade build script

--- tools/Docker/build.sh
+++ tools/Docker/build.sh
@@ -1,1 +1,26 @@
-sudo docker build -t auavlinux build/. --rm
+gpu=
+usage="bash build.sh [-h][-g n] -- builds the Docker image with source code and all needed libraries
+
+Flags:
+	-h, --help	show this help text
+	-g, --gpu	compiles Nvidia GPU compatible version"
+	
+while [ "$1" != "" ]; do
+	case $1 in 
+		-g | --gpu )	shift
+				gpu=1
+				;;
+		-h | --help )	echo "$usage"
+				exit
+				;;
+		*)		echo "$usage"
+		       		exit
+				;;	
+	esac
+	shift
+done
+if [ "$gpu" = "1" ]; then
+	sudo docker build -t auavlinux/nvidia -f build/Dockerfile.nvidia build/. --rm
+else
+	sudo docker build -t auavlinux -f build/Dockerfile build/. --rm
+fi
