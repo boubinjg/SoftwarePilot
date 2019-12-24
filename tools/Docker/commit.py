2292e71b66fbae38ca92a519f61609148a2434ef
boubinjg <boubinjg@gmail.com>
2019-12-24 15:56:48 -0500

    Added docker commit script

---
+++ tools/Docker/commit.py
@@ -0,0 +1,14 @@
+import subprocess
+
+op = subprocess.check_output(["sudo","docker", "ps","-a"])
+op = op.split("\n")
+for i in op:
+    i = i.split()
+    try:
+        if(i[1] == "auavlinux"):
+            print([i[0], i[1]])
+            op = subprocess.check_output(["sudo","docker","commit",i[0], i[1]])
+            print(op)
+            break;
+    except:
+        pass
