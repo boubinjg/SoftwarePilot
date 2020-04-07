7f9203f609d76f0efdc44eb6c39ca6592554f6e4
boubinjg <boubinjg@gmail.com>
2020-04-07 17:12:29 -0400

    Update on Cassandra

--- DistributedRL/CentralNode/runCN.sh
+++ DistributedRL/CentralNode/runCN.sh
@@ -1,1 +1,1 @@
-sudo docker run -v /CNData:/var/lib/cassandra -it -p 5117:5117/udp -p 12013:12013 -p 6379:6379 spcn /bin/bash
+sudo docker run -v /CNData:/var/lib/cassandra -it -p 5117:5117/udp -p 12013:12013 -p 0.0.0.0:9042:9042 -p 0.0.0.0:9160:9160 --network "host" spcn /bin/bash 
