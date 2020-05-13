66e381b927aa214d39e9e4ecaca448176faaf41c
boubinjg <root@chris04.cse.ohio-state.edu>
2020-05-13 16:11:21 -0400

    DRL Updates distributing from servers to workers

--- DistributedRL/EdgeNode/build/Code/sim/Controller.py
+++ DistributedRL/EdgeNode/build/Code/sim/Controller.py
@@ -3,6 +3,29 @@ import subprocess
 from os import listdir
 from os.path import isfile, join
 
+def checkForUpdate(count, lastUpdate):
+    sn = os.environ['SERVERNUM']
+    updateCount = -1
+    print([count, lastUpdate])
+    for i in range(lastUpdate+1, count):
+        print("In Check "+str(i))
+        out = os.popen('/opt/hadoop/bin/hadoop fs -test -e hdfs://127.0.0.1:9000/server_'+
+              sn+'/run_'+str(i)+'/knndata && echo $?').read()
+        try:
+            if(int(out) == 0):
+                updateCount = i
+                print("Update Ready: "+str(i))
+        except:
+            break;
+            print("Update Not Ready")
+    
+    if(updateCount > -1):
+        os.remove('knndatasetGI')
+        out = os.popen('/opt/hadoop/bin/hadoop fs -copyToLocal hdfs://127.0.0.1:9000/server_'+
+              sn+'/run_'+str(updateCount)+'/knndata knndatasetGI').read()
+
+    return updateCount
+
 cwd = os.getcwd()
 csvs = []
 os.chdir("/home/mydata/")
@@ -20,8 +43,12 @@ except Exception as e:
     print(e)
 
 count = 0
+lastUpdate = -1
 
 for f in csvs:
+    print("wow");
+    lastUpdate = checkForUpdate(count, lastUpdate)
+    
     subprocess.call(['bash','runGI.bash','/home/mydata/'+f,'10', str(count)])
     os.chdir('tmp/')
 
@@ -32,4 +59,6 @@ for f in csvs:
         print(x)
     subprocess.call(['/opt/hadoop/bin/hadoop','fs','-touchz','hdfs://127.0.0.1:9000/worker'+sn+'_'+wn+'/run_'+str(count)+"/done"])
     os.chdir(cwd)
+    
     count += 1
+
--- DistributedRL/EdgeNode/build/Code/sim/runGI.bash
+++ DistributedRL/EdgeNode/build/Code/sim/runGI.bash
@@ -1,4 +1,4 @@
 file=$1
 thold=$2
 seed=$3
-python3 SimGI.py $file /home/mydata/knndatasetGI $thold $seed
+python3 SimGI.py $file knndatasetGI $thold $seed
