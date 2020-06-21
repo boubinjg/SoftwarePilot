9e697e15504a2d50605c92c0bc486037ca96d6e4
boubinjg <root@chris04.cse.ohio-state.edu>
2020-06-21 12:10:12 -0400

    DRL Updates

--- DistributedRL/DRLController/reset.sh
+++ DistributedRL/DRLController/reset.sh
@@ -1,10 +1,10 @@
-docker rm -f worker1_1
-docker rm -f worker0_1
-docker rm -f worker1_0
-docker rm -f worker0_0
-docker rm -f server1
-docker rm -f server0
-docker rm -f global
+sudo docker rm -f worker1_1
+sudo docker rm -f worker0_1
+sudo docker rm -f worker1_0
+sudo docker rm -f worker0_0
+sudo docker rm -f server1
+sudo docker rm -f server0
+sudo docker rm -f global
 hadoop dfsadmin -safemode leave
 hadoop fs -rm -r hdfs://127.0.0.1:9000/*
-rm -f ../Data/Worker*/run_*
+sudo rm -f ../Data/Worker*/run_*
